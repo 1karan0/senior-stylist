a9ab5e31698bd244904bb6009479fca690c682b6
1karan0 <karan523thakur@gmail.com>
2025-12-08 13:05:26 +0530

    feat: added the splash screen

--- android/app/src/main/java/com/senior_stylist/splashScreen.kt
+++ android/app/src/main/java/com/senior_stylist/splashScreen.kt
@@ -1,6 +1,7 @@
 package com.senior_stylist
 
 import android.os.Bundle
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
